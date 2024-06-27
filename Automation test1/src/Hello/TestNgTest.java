cf28884508bb232daee11f408eb8fd0761ce2f2b
AbhishekChaudhary22 <abheechaudharyy@gmail.com>
2024-06-27 23:19:45 +0530

    chal ja bhai

---
+++ Automation test1/src/Hello/TestNgTest.java
@@ -0,0 +1,13 @@
+package Hello;
+
+import org.junit.Test;
+
+public class TestNgTest {
+
+    public void testMethod() {
+
+    System.out.println("testng method");
+
+    }
+
+}
---
+++ Automation test1/src/org/junit/TestNgTest.java
@@ -0,0 +1,10 @@
+package org.junit;
+
+public class TestNgTest {
+	
+	@Test
+	public void testMethod() {
+	System.out.println("testing method");
+	}
+
+}
